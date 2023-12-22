b98d088e8724698be657e121872fbef91485344b
abdirahman <abdixcom@gmail.com>
2023-12-22 20:21:55 +0300

    added transferTransaction

--- javascript/transferTransaction.js
+++ javascript/transferTransaction.js
@@ -1,9 +1,9 @@
-function basicTransactionData() {
+function transferTransactionData() {
     const tab = document.getElementById('tab')
 
     const userId = JSON.parse(localStorage.getItem('kmbankUser')).id
 
-    const url = `http://localhost:8080/api/v1/basicTransaction/${userId}`
+    const url = `http://localhost:8080/api/v1/transferTransaction/${userId}`
     fetch(url).then(res => res.json()).then(data => {
         data.forEach(element => {
             const obj = document.createElement('div')
@@ -14,4 +14,4 @@ function basicTransactionData() {
     })
 }
 
-basicTransactionData()
+transferTransactionData()

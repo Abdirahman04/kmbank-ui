609a58bb1f93bddcbbc95f83dbd39419e919e5b8
abdirahman <abdixcom@gmail.com>
2023-12-22 17:08:27 +0300

    added some code

--- javascript/deposit.js
+++ javascript/deposit.js
@@ -2,15 +2,18 @@ function depositData() {
     const user = JSON.parse(localStorage.getItem('kmbankUser'))
 
     const btn = document.getElementById('btn')
-    const amount = document.getElementById('amount')
-    const pass = document.getElementById('pass')
     const err = document.getElementById('err')
 
     btn.addEventListener('click', (event) => {
         event.preventDefault()
 
-        if (!verifyPassword(user.accountNumber, pass)) err.innerHTML = "incorrect password!"
-        else depositHandler(user.id, user.accountNumber, amount)
+        const amount = document.getElementById('amount').value
+        const pass = document.getElementById('pass').value
+
+        verifyPassword(user.accountNumber, pass).then(data => {
+            if (!data) err.innerHTML = "incorrect password!"
+            else depositHandler(user.id, user.accountNumber, amount)
+        })
     })
 }
 
@@ -29,11 +32,11 @@ function verifyPassword(accNumber, pass) {
         body: JSON.stringify(bdd)
     }
 
-    fetch(url, fetchOptions)
+    return fetch(url, fetchOptions)
     .then(res => res.json())
 }
 
-function depositHandler(id, balance, accNumber) {
+function depositHandler(id, accNumber, balance) {
     const bdd = {
         id: id,
         balance: balance
@@ -52,16 +55,17 @@ function depositHandler(id, balance, accNumber) {
     .then(res => {
         if (!res.ok) throw new Error(`HTTP error: status ${res.status}`)
         else {
-            updateLocalStorage(accNumber)
-            window.location.href = "home.html"
+            updateLocalStorage(accNumber).then(data => {
+                localStorage.setItem('kmbankUser', JSON.stringify(data))
+                window.location.href = "home.html"
+            })
         }
     })
 }
 
 function updateLocalStorage(accNumber) {
-    fetch(`http://localhost:8080/api/v1/account/${accNumber}`)
+    return fetch(`http://localhost:8080/api/v1/user/account/${accNumber}`)
     .then(res => res.json())
-    .then(data => localStorage.setItem('kmbankUser', JSON.stringify(data)))
 }
 
 depositData()

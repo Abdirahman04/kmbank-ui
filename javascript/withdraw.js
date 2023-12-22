70f78b306dc56d5ff9cbfbff1dc352c6315067e0
abdirahman <abdixcom@gmail.com>
2023-12-22 17:18:12 +0300

    added withdraw.js

---
+++ javascript/withdraw.js
@@ -0,0 +1,71 @@
+function depositData() {
+    const user = JSON.parse(localStorage.getItem('kmbankUser'))
+
+    const btn = document.getElementById('btn')
+    const err = document.getElementById('err')
+
+    btn.addEventListener('click', (event) => {
+        event.preventDefault()
+
+        const amount = document.getElementById('amount').value
+        const pass = document.getElementById('pass').value
+
+        verifyPassword(user.accountNumber, pass).then(data => {
+            if (!data) err.innerHTML = "incorrect password!"
+            else withdrawHandler(user.id, user.accountNumber, amount)
+        })
+    })
+}
+
+function verifyPassword(accNumber, pass) {
+    const bdd = {
+        accountNumber: accNumber,
+        password: pass
+    }
+
+    const url = "http://localhost:8080/api/v1/user/login";
+    const fetchOptions = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(bdd)
+    }
+
+    return fetch(url, fetchOptions)
+    .then(res => res.json())
+}
+
+function withdrawHandler(id, accNumber, balance) {
+    const bdd = {
+        id: id,
+        balance: balance
+    }
+
+    const url = "http://localhost:8080/api/v1/withdraw"
+    const fetchOptions = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(bdd)
+    }
+
+    fetch(url, fetchOptions)
+    .then(res => {
+        if (!res.ok) throw new Error(`HTTP error: status ${res.status}`)
+        else {
+            updateLocalStorage(accNumber).then(data => {
+                localStorage.setItem('kmbankUser', JSON.stringify(data))
+                window.location.href = "home.html"
+            })
+        }
+    })
+}
+
+function updateLocalStorage(accNumber) {
+    return fetch(`http://localhost:8080/api/v1/user/account/${accNumber}`)
+    .then(res => res.json())
+}
+
+depositData()

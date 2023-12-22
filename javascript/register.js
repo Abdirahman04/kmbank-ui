58ed734e856da3571c247f2e7a4305e5a39dfcac
abdirahman <abdixcom@gmail.com>
2023-12-22 21:08:24 +0300

    added register.js

---
+++ javascript/register.js
@@ -0,0 +1,45 @@
+function register() {
+    const btn = document.getElementById('btn')
+    const err = document.getElementById('err')
+
+    const url = "http://localhost:8080/api/v1/user"
+
+    btn.addEventListener('click', (event) => {
+        event.preventDefault()
+
+        const bdd = {
+            accountNumber: document.getElementById('acc').value,
+            firstName: document.getElementById('fname').value,
+            lastName: document.getElementById('lname').value,
+            dob: document.getElementById('dob').value,
+            email: document.getElementById('email').value,
+            password: document.getElementById('pass').value
+        }
+
+        const options = {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(bdd)
+        }
+
+        console.log(bdd);
+
+        const accNumber = document.getElementById('acc').value
+        fetch(url, options).then(res => {
+            if (!res.ok) err.innerHTML = "Error creating account!"
+            else {
+                updateLocalStorage(accNumber).then(data => {
+                    localStorage.setItem('kmbankUser', JSON.stringify(data))
+                    window.location.href = "home.html"
+                })
+            }
+        })
+    })
+}
+
+function updateLocalStorage(accNumber) {
+    return fetch(`http://localhost:8080/api/v1/user/account/${accNumber}`)
+    .then(res => res.json())
+}
+
+register()
